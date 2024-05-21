4c84dc1e5c8c85543339843de149f9dad3c71118
edmundnche <edmundche.nche@edu.sait.ca>
2024-05-21 17:12:55 -0600

    Final Submission

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+        <>
+            <div>
+                <p>Edmund Che Nche</p>
+                <p>
+                    <Link href="https://github.com/edmundnche/cprg306-assignments.git">My GitHub Link</Link>
+                </p>
+            </div>
+        </>
+
+    );
+}
