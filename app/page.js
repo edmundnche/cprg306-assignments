c1bd9a86fe9fca432f28d87373c66ffde6f7f9e1
edmundnche <edmundche.nche@edu.sait.ca>
2024-05-28 22:02:29 -0600

    Week 3 assignment update

--- app/page.js
+++ app/page.js
@@ -1,14 +1,20 @@
 import Link from 'next/link';
 
 export default function HomePage() {
-  const weekDemo = 2;
+  const weekAssignments = [2, 3];
+  const doneUpTo = 2;
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-26">
       <div className="z-12 max-w-5xl w-full items-center justify-between text-sm">
         <h1 className="text-5xl font-bold mb-6">CPRG 306: Web Development 2 - Assignments</h1>
         <div className="text-lg">
           <ul>
-            <Link href="/week-2"> <a>Week 2 Assignment</a></Link>
+            {weekAssignments.map((week) => (
+              <li key={week} className="hover:text-blue-400 hover:underline">
+                <Link href={`/week-${week}`}>Week {week} Assignment</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
